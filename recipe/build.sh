740d26fe633b7354fce0f77f9ee0e93913c582df
Morten Enemark Lund <molu@nykredit.it>
2026-03-25 21:29:39 +0100

    Fix paths in build.sh to correctly reference local directories for file movement

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,13 +2,14 @@
 set -ex
 
 # conda-build extracts the RPM automatically into SRC_DIR
+# It strips the top-level "usr/" directory, so files are at local/...
 
 mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/include
 mkdir -p $PREFIX/lib
 mkdir -p $PREFIX/share/man/man1/
 
-mv usr/local/bin/* $PREFIX/bin/
-mv usr/local/include/wkhtmltox/ $PREFIX/include/
-mv usr/local/lib/* $PREFIX/lib/
-mv usr/local/share/man/man1/* $PREFIX/share/man/man1/
+mv local/bin/* $PREFIX/bin/
+mv local/include/wkhtmltox/ $PREFIX/include/
+mv local/lib/* $PREFIX/lib/
+mv local/share/man/man1/* $PREFIX/share/man/man1/

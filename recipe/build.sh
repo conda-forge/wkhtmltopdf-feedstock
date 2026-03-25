e6cdcbe8c489acbe9f2c39118dc9949fa620d2a5
Morten Enemark Lund <molu@nykredit.it>
2026-03-25 22:10:06 +0100

    Build new updated 0.12.4 with extra win package

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-set -ex
-
-# conda-build extracts the RPM automatically into SRC_DIR
-# It strips the top-level "usr/" directory, so files are at local/...
 
 mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/include
 mkdir -p $PREFIX/lib
 mkdir -p $PREFIX/share/man/man1/
 
-mv local/bin/* $PREFIX/bin/
-mv local/include/wkhtmltox/ $PREFIX/include/
-mv local/lib/* $PREFIX/lib/
-mv local/share/man/man1/* $PREFIX/share/man/man1/
+
+mv bin/* $PREFIX/bin/
+mv include/wkhtmltox/ $PREFIX/include/
+mv lib/* $PREFIX/lib/
+mv share/man/man1/* $PREFIX/share/man/man1/
